36f48f56c191a91d1abf29e9c61f76eb89af83ad
Natanael Rolim <natanael.10@hotmail.com>
2023-10-22 21:35:42 -0300

    first commit

---
+++ src/main/java/dio/digitalinnovation/lab/padroes/projetos/spring/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package dio.digitalinnovation.lab.padroes.projetos.spring.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import dio.digitalinnovation.lab.padroes.projetos.spring.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
+}
---
+++ src/test/java/dio/digitalinnovation/lab/padroes/projetos/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.digitalinnovation.lab.padroes.projetos.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
